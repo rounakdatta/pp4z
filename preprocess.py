1e74d9cf95342bafbc8c6d1744717cbe6a3fd793
Rounak Datta <rounakdatta12@gmail.com>
2021-04-12 23:46:14 +0530

    solve certain issues

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@
 
 import csv
 import click
+import datetime
 
 def get_field_column(header_row, field_name):
     return header_row.index(field_name)
@@ -19,15 +20,20 @@ def get_updated_ticker_symbol(original_ts, exchange):
 
     return original_ts
 
-def process_csv(csv_in, csv_out):
+def process_csv(csv_in, csv_out, use_exchange):
     header_row = next(csv_in)
+
     ticker_symbol_column = get_field_column(header_row, "symbol")
     exchange_column = get_field_column(header_row, "exchange")
     quantity_column = get_field_column(header_row, "quantity")
     price_column = get_field_column(header_row, "price")
+    trade_date_column = get_field_column(header_row, "trade_date")
+    trade_type_column = get_field_column(header_row, "trade_type")
+
+    updated_rows = []
 
     for row in csv_in:
-        exchange = row[exchange_column]
+        exchange = use_exchange if use_exchange != "None" else row[exchange_column]
         ticker_symbol = row[ticker_symbol_column]
         updated_row = update_field_in_row(
             row,
@@ -35,6 +41,7 @@ def process_csv(csv_in, csv_out):
             get_updated_ticker_symbol(ticker_symbol, exchange)
         )
 
+        # the value column is expected to be the total exchange value
         quantity = row[quantity_column]
         price = row[price_column]
         updated_row = update_field_in_row(
@@ -43,18 +50,32 @@ def process_csv(csv_in, csv_out):
             float(price)*float(quantity)
         )
 
-        csv_out.writerow(updated_row)
+        updated_rows.append(updated_row)
+
+    # PP doesn't support short selling while importing, so we need to keep buy above sell
+    updated_sorted_rows = sorted(
+        updated_rows,
+        key=lambda x: (
+            datetime.datetime.strptime(x[trade_date_column], "%Y-%m-%d"),
+            x[ticker_symbol_column],
+            x[trade_type_column]
+        )
+    )
+
+    updated_sorted_rows = [header_row] + updated_sorted_rows
+    csv_out.writerows(updated_sorted_rows)
 
 @click.command()
 @click.option("--input_csv", prompt="Input file (tradebook csv)")
 @click.option("--output_csv", prompt="Output file (for outputting compatible tradebook csv)")
-def init(input_csv, output_csv):
+@click.option("--use_exchange", prompt="Fixed exchange to use for all trades", type=click.Choice(["NSE", "BSE", "None"]), default="None")
+def init(input_csv, output_csv, use_exchange):
     in_file = open(input_csv, "r")
     out_file = open(output_csv, mode="w")
     csv_in = csv.reader(in_file, delimiter=",")
     csv_out = csv.writer(out_file, delimiter=",")
 
-    process_csv(csv_in, csv_out)
+    process_csv(csv_in, csv_out, use_exchange)
 
 if __name__ == '__main__':
     init()
